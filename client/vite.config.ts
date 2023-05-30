79ef6aba639d72de2fb2e664fcea5bc66c0c165b
Jack Reichert <awesome@jackreichert.com>
2023-05-30 17:46:07 -0400

    adds vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
+
+type JSXConfig = {
+  plugins: PluginOption[][];
+} & {
+  test: {
+    environment: string;
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
+} as JSXConfig);

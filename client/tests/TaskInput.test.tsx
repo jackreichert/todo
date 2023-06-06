e52f435bc23e080911a80ceeffe6ec3202f48f9c
Jack Reichert <awesome@jackreichert.com>
2023-06-06 17:54:09 -0400

    adds test coverage to 100%

--- client/tests/TaskInput.test.tsx
+++ client/tests/TaskInput.test.tsx
@@ -10,13 +10,22 @@ describe('Input', () => {
         expect(getByPlaceholderText('add your next task to do')).toBeInTheDocument();
     });
 
-    test('input field test', () => {
+    test('input field test with click', () => {
         const {getByTestId} = render(<TaskInput/>);
         const input = getByTestId('task-input') as HTMLInputElement;
         const button = getByTestId('task-submit') as HTMLButtonElement;
         fireEvent.change(input, {target: {value: 'test'}});
         expect(input.value).toBe('test');
         fireEvent.click(button)
+        expect(input.value).toBe('');
+    });
+
+    test('input field test with enter', () => {
+        const {getByTestId} = render(<TaskInput/>);
+        const input = getByTestId('task-input') as HTMLInputElement;
+        fireEvent.change(input, {target: {value: 'test'}});
+        expect(input.value).toBe('test');
+        fireEvent.keyDown(input, { key: 'Enter', code: 13, charCode: 13 });
         expect(input.value).toBe('');
     });
 });

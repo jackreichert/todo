79eafd030d17a8a010af28fca255784ab365a752
Jack Reichert <awesome@jackreichert.com>
2023-07-22 17:41:19 -0400

    adds graphql endpoint

--- server/app/app.py
+++ server/app/app.py
@@ -1,9 +1,44 @@
-from flask import Flask
+from ariadne import QueryType, graphql_sync, make_executable_schema
+from ariadne.explorer import ExplorerGraphiQL
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+
+
+from .types import *
+
+query = QueryType()
+
+
+@query.field("tasks")
+def resolve_hello(_, info):
+    return tasks
+
+
+schema = make_executable_schema(type_defs, query)
 
 app = Flask(__name__)
+CORS(app)
+explorer_html = ExplorerGraphiQL().html(None)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-    
+
+@app.route("/graphql", methods=["GET"])
+def graphql_explorer():
+    return explorer_html, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value={"request": request},
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
 application = app

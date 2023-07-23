c049b444d2156a422968c68f56fef68e7472a706
Jack Reichert <awesome@jackreichert.com>
2023-07-23 17:40:47 -0400

    adds delete and edit buttons

--- client/src/components/Task/index.tsx
+++ client/src/components/Task/index.tsx
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { TaskType } from '../../types';
 import AppContext from '../../context';
 
 export default function Task({ title, status }: TaskType) {
   const { tasks, setTasks } = useContext(AppContext);
+  const [isEdit, setIsEdit] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
 
-  function handleChange() {
+  function handleStatusChange() {
     const newTasks: TaskType[] = tasks.map((task) => (title === task.title ? {
       title: task.title,
       status: !task.status,
@@ -13,18 +15,47 @@ export default function Task({ title, status }: TaskType) {
     setTasks(newTasks);
   }
 
-  function handleDelete(e:React.MouseEvent<HTMLButtonElement>) {
-    let liTitle = (e.target as HTMLElement).parentNode?.textContent || '';
-    liTitle = liTitle.slice(0, -10);
-    const newTasks: TaskType[] = tasks.filter((task) => liTitle !== task.title);
+  function handleDelete() {
+    const newTasks: TaskType[] = tasks.filter((task) => title !== task.title);
     setTasks(newTasks);
   }
 
+  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewTitle(event.target.value);
+  }
+
+  function handleEdit() {
+    setIsEdit(!isEdit);
+    setNewTitle(title);
+  }
+
+  function handleSave() {
+    const newTasks: TaskType[] = tasks.map((task) => (title === task.title ? {
+      title: newTitle,
+      status: task.status,
+    } : task));
+    setTasks(newTasks);
+    setIsEdit(!isEdit);
+  }
+
+  function handleCancel() {
+    setIsEdit(!isEdit);
+  }
+
+  if (isEdit) {
+    return (
+      <li className="list-none my-4 flex justify-between">
+        <input type="text" onChange={handleTitleChange} value={newTitle} />
+        <button type="button" onClick={handleSave}>Save</button>
+        <button type="button" onClick={handleCancel}>Cancel</button>
+      </li>
+    );
+  }
   return (
     <li className="list-none my-4 flex justify-between">
-      {title}
-      <input type="checkbox" checked={status} onChange={handleChange} />
-      <button type="button">Edit</button>
+      <span data-testid="task-title">{title}</span>
+      <input type="checkbox" checked={status} onChange={handleStatusChange} />
+      <button type="button" onClick={handleEdit}>Edit</button>
       <button type="button" onClick={handleDelete}>Delete</button>
     </li>
   );
--- client/tests/Task.test.tsx
+++ client/tests/Task.test.tsx
@@ -27,19 +27,6 @@ describe('Task', () => {
     expect(checkboxes[1]).toBeChecked();
   });
 
-  it('has an edit button', () => {
-    const { getByRole } = render(<MockProvider />);
-
-    const list = getByRole('list');
-    const { getAllByRole } = within(list);
-
-    const items = getAllByRole('listitem');
-    const button = getAllByRole('button', { container: items } as ByRoleOptions);
-
-    expect(button[0]).toBeDefined();
-    expect(button[0]).toHaveTextContent('Edit');
-  });
-
   it('deletes when delete is clicked', () => {
     const { getByRole } = render(<MockProvider />);
 
@@ -56,5 +43,102 @@ describe('Task', () => {
     fireEvent.click(button[1]);
     items = getAllByRole('listitem');
     expect(items[0]).not.toHaveTextContent('Go to the store');
+  });
+
+  it('has an edit button', () => {
+    const { getByRole } = render(<MockProvider />);
+
+    const list = getByRole('list');
+    const { getAllByRole } = within(list);
+
+    const items = getAllByRole('listitem');
+    const button = getAllByRole('button', { container: items } as ByRoleOptions);
+
+    expect(button[0]).toBeDefined();
+    expect(button[0]).toHaveTextContent('Edit');
+  });
+
+  it('when edit is clicked, makes title editable, and adds buttons for editing', () => {
+    const { getByRole } = render(<MockProvider />);
+
+    const list = getByRole('list');
+    const { getAllByRole } = within(list);
+
+    let items = getAllByRole('listitem');
+    let buttons = getAllByRole('button', { container: items } as ByRoleOptions);
+
+    expect(buttons[0]).toBeDefined();
+    expect(buttons[0]).toHaveTextContent('Edit');
+    fireEvent.click(buttons[0]);
+    items = getAllByRole('listitem');
+    const inputs = getAllByRole('textbox', { container: items[0] } as ByRoleOptions);
+    expect(inputs).toBeDefined();
+    buttons = getAllByRole('button', { container: items[0] } as ByRoleOptions);
+    expect(buttons[0]).not.toHaveTextContent('Edit');
+    expect(buttons[0]).toHaveTextContent('Save');
+    expect(buttons[1]).toHaveTextContent('Cancel');
+  });
+
+  it('when cancel is clicked, title reverts to original title', () => {
+    const { getByRole } = render(<MockProvider />);
+
+    const list = getByRole('list');
+    const { getAllByRole } = within(list);
+
+    let items = getAllByRole('listitem');
+    let buttons = getAllByRole('button', { container: items } as ByRoleOptions);
+    const { getByTestId } = within(items[0]);
+    let title = getByTestId('task-title') as HTMLInputElement;
+    expect(title).toHaveTextContent('Go to the store');
+
+    expect(buttons[0]).toBeDefined();
+    expect(buttons[0]).toHaveTextContent('Edit');
+    fireEvent.click(buttons[0]);
+    items = getAllByRole('listitem');
+
+    const inputs = getAllByRole('textbox', { container: items[0] } as ByRoleOptions);
+    expect(inputs).toBeDefined();
+
+    buttons = getAllByRole('button', { container: items[0] } as ByRoleOptions);
+    expect(buttons[1]).toBeDefined();
+    expect(buttons[1]).toHaveTextContent('Cancel');
+
+    fireEvent.change(inputs[0], { target: { value: 'Do not go to the store' } });
+    fireEvent.click(buttons[1]);
+    items = getAllByRole('listitem');
+    title = getByTestId('task-title') as HTMLInputElement;
+    expect(title).toHaveTextContent('Go to the store');
+  });
+
+  it('when save is clicked, title is updated', () => {
+    const { getByRole } = render(<MockProvider />);
+
+    const list = getByRole('list');
+    const { getAllByRole } = within(list);
+
+    let items = getAllByRole('listitem');
+    let buttons = getAllByRole('button', { container: items } as ByRoleOptions);
+    const { getByTestId: beforeGetByTestId } = within(items[0]);
+    let title = beforeGetByTestId('task-title') as HTMLInputElement;
+    expect(title).toHaveTextContent('Go to the store');
+
+    expect(buttons[0]).toBeDefined();
+    expect(buttons[0]).toHaveTextContent('Edit');
+    fireEvent.click(buttons[0]);
+
+    items = getAllByRole('listitem');
+    const inputs = getAllByRole('textbox', { container: items[0] } as ByRoleOptions);
+    expect(inputs).toBeDefined();
+
+    buttons = getAllByRole('button', { container: items[0] } as ByRoleOptions);
+    expect(buttons[0]).toBeDefined();
+    expect(buttons[0]).toHaveTextContent('Save');
+
+    fireEvent.change(inputs[0], { target: { value: 'Do not go to the store' } });
+    fireEvent.click(buttons[0]);
+    items = getAllByRole('listitem');
+    const { getByTestId: afterGetByTestId } = within(items[0]);
+    title = afterGetByTestId('task-title') as HTMLInputElement;
+    expect(title).toHaveTextContent('Do not go to the store');
   });
 });

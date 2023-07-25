2e02fb6f74ddfc06cde42e15b8d679e074cbc21c
Jack Reichert <awesome@jackreichert.com>
2023-07-25 11:06:01 -0400

    playing with tests and mocks with tmosk

--- client/src/AppProvider.tsx
+++ client/src/AppProvider.tsx
@@ -1,14 +1,19 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import './App.scss';
 
 import { TaskType } from './types';
 import AppContext from './context';
 import App from './components/App';
+import { getTasks } from './utils';
 
 function AppProvider() {
   const [tasks, setTasks] = useState<TaskType[]>([]);
 
   const contextValue = useMemo(() => ({ tasks, setTasks }), [tasks, setTasks]);
+
+  useEffect(() => {
+    getTasks(setTasks);
+  }, [setTasks]);
 
   return (
     <div className="container mx-auto">

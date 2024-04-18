bbac24750d47ea8d490efb652833280fbd5e05cc
Jack Reichert <awesome@jackreichert.com>
2024-04-18 11:24:49 -0400

    updates @testing-library/jest-dom

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './tests/setup.js',
+    setupFiles: './tests/setup.ts',
   },
   server: {
     port: 5173,

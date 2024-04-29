7e81b07d900a838f33ddda047839f2e2b45e5edc
Jack Reichert <awesome@jackreichert.com>
2024-04-29 11:08:47 -0400

    adding types

--- backend/resources/list.py
+++ backend/resources/list.py
@@ -1,3 +1,4 @@
+from typing import List, Dict, Any
 from uuid import uuid4 as UUID4
 from flask.views import MethodView
 from flask_smorest import Blueprint
@@ -9,11 +10,13 @@ blp = Blueprint('List', 'list', url_prefix='/list', description='List operations
 
 @blp.route('/', methods=['GET', 'POST'])
 class ListView(MethodView):
-    def get(self):
+    @blp.response(200, ListSchema(many=True))
+    def get(self) -> List[Dict[str, Any]]:
         return db.lists
 
     @blp.arguments(ListSchema)
-    def post(self, list_data):
+    @blp.response(201, ListSchema(many=True))
+    def post(self, list_data: Dict[str, Any]) -> List[Dict[str, Any]]:
         list_data['id'] = str(UUID4())
         db.lists.append(list_data)
         return db.lists
--- backend/resources/task.py
+++ backend/resources/task.py
@@ -1,5 +1,6 @@
+from typing import List, Dict, Any
 from uuid import uuid4 as UUID4
-from flask import jsonify
+from flask import jsonify, Response
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
@@ -10,11 +11,13 @@ blp = Blueprint('Task', 'task', url_prefix='/task', description='Task operations
 
 @blp.route('/<string:list_id>', methods=['GET', 'POST'])
 class TaskView(MethodView):
-    def get(self, list_id):
-        return [task['list_id'] == list_id for task in db.tasks]
+    @blp.response(200, TaskSchema(many=True))
+    def get(self, list_id: str) -> List[Dict[str, Any]]:
+        return [task for task in db.tasks if task['list_id'] == list_id]
 
-    @blp.arguments(TaskSchema)
-    def post(self, task_data, list_id):
+    @blp.arguments(TaskSchema, location="json")
+    @blp.response(201, TaskSchema(many=True))
+    def post(self, task_data: Dict[str, Any], list_id: str) -> tuple[Response, int] | Any:
         if not any(str(list_id) == str(l['id']) for l in db.lists):
             return jsonify('List not found'), 404
 

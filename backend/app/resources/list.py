0df3bf92e47f6e44bf08b1654e25092422f1c7c2
Jack Reichert <awesome@jackreichert.com>
2024-04-30 10:55:33 -0400

    restructures endpoints

--- backend/app/resources/list.py
+++ backend/app/resources/list.py
@@ -1,12 +1,15 @@
 from typing import List, Dict, Any
 from uuid import uuid4 as UUID4
+from flask import jsonify, Response
 from flask.views import MethodView
 from flask_smorest import Blueprint
 
 from backend.app import db
 from backend.app.schemas.list import ListSchema
+from backend.app.schemas.task import TaskSchema
 
 blp = Blueprint('List', 'list', url_prefix='/list', description='List operations')
+
 
 @blp.route('/', methods=['GET', 'POST'])
 class ListView(MethodView):
@@ -20,3 +23,22 @@ class ListView(MethodView):
         list_data['id'] = str(UUID4())
         db.lists.append(list_data)
         return db.lists
+
+
+@blp.route('/<string:list_id>', methods=['GET', 'PUT', 'DELETE'])
+class ListDetailView(MethodView):
+    @blp.response(200, ListSchema)
+    def get(self, list_id: str) -> Dict[str, Any]:
+        return next(filter(lambda l: l['id'] == list_id, db.lists))
+
+    @blp.arguments(ListSchema, location="json")
+    @blp.response(200, ListSchema)
+    def put(self, list_data: Dict[str, Any], list_id: str) -> Dict[str, Any]:
+        list_data['id'] = list_id
+        db.lists = [list_data if l['id'] == list_id else l for l in db.lists]
+        return list_data
+
+    @blp.response(204)
+    def delete(self, list_id: str) -> None:
+        db.lists = [l for l in db.lists if l['id'] != list_id]
+        return None
--- backend/app/resources/task.py
+++ backend/app/resources/task.py
@@ -9,19 +9,20 @@ from backend.app.schemas.task import TaskSchema
 
 blp = Blueprint('Task', 'task', url_prefix='/task', description='Task operations')
 
-@blp.route('/<string:list_id>', methods=['GET', 'POST'])
+
+@blp.route('/', defaults={'task_id': None}, methods=['GET', 'POST'])
+@blp.route('/<string:task_id>', methods=['GET', 'POST'])
 class TaskView(MethodView):
     @blp.response(200, TaskSchema(many=True))
-    def get(self, list_id: str) -> List[Dict[str, Any]]:
-        return [task for task in db.tasks if task['list_id'] == list_id]
+    def get(self, task_id: str) -> List[Dict[str, Any]]:
+        return filter(lambda task: task['id'] == task_id, db.tasks)
 
     @blp.arguments(TaskSchema, location="json")
     @blp.response(201, TaskSchema(many=True))
-    def post(self, task_data: Dict[str, Any], list_id: str) -> tuple[Response, int] | Any:
-        if not any(str(list_id) == str(l['id']) for l in db.lists):
+    def post(self, task_data: Dict[str, Any], task_id: str) -> tuple[Response, int] | Any:
+        if not any(str(task_data['list_id']) == str(l['id']) for l in db.lists):
             return jsonify('List not found'), 404
 
         task_data['id'] = str(UUID4())
-        task_data['list_id'] = list_id
         db.tasks.append(task_data)
         return db.tasks

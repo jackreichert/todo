942d4a9f48091bbb6352f22298f175f120a3b755
Jack Reichert <awesome@jackreichert.com>
2024-04-30 13:49:42 -0400

    improves tests

--- backend/tests/test_list.py
+++ backend/tests/test_list.py
@@ -1,23 +1,30 @@
-import pytest
 
 
-def test_create_list(client):
-    title = "Test List"
-    response = client.post(
-        "/list/",
-        json={"title": title},
-    )
-    assert response.status_code == 201
-    assert isinstance(response.json, list)
-    assert "id" in response.json[0]
-    assert response.json[0]["title"] == title
+class TestListCreation:
+    def test_create_list(self, client):
+        title = "Test List"
+        response = client.post(
+            "/list/",
+            json={"title": title},
+        )
+        assert response.status_code == 201
+        assert isinstance(response.json, list)
+        assert "id" in response.json[0]
+        assert response.json[0]["title"] == title
 
-    response = client.get(
-        "/list/"
-    )
+    def test_get_list(self, client):
+        title = "Another List"
+        client.post(
+            "/list/",
+            json={"title": title},
+        )
+        response = client.get(
+            "/list/"
+        )
 
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
-    assert len(response.json) == 1
-    assert "id" in response.json[0]
-    assert response.json[0]["title"] == title
+        assert response.status_code == 200
+        assert isinstance(response.json, list)
+        assert len(response.json) == 2
+        assert "id" in response.json[0]
+        assert "id" in response.json[1]
+        assert response.json[1]["title"] == title
